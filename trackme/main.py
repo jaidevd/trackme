5368b4bab8d73a6a2829c993f85663046a7ecc44
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2014-09-05 19:21:02 +0530

    Add method for github issues

--- trackme/main.py
+++ trackme/main.py
@@ -3,9 +3,14 @@ from getpass import getpass
 import feedparser
 import sys
 import datetime
+import dateutil.parser
 import time
 
 fmt = "%A %d. %B"
+
+ISSUES_QUERY = ("https://api.github.com/orgs/{org}/issues"
+                "?filter=subscribed&state=all")
+
 
 def get_repos_list(auth):
     s = requests.session()
@@ -20,7 +25,33 @@ def get_repos_list(auth):
             break
     return repo_list
 
+
+def get_issues_data(org, auth):
+    print "="*25 + " Issues feed " + "="*25
+    s = requests.session()
+    url = ISSUES_QUERY.format(org=org)
+    r = s.get(url, auth=auth)
+    issues = r.json()
+    for issue in issues:
+        title = issue['title']
+        if issue['user']['login'] == auth[0]:
+            created = issue['created_at']
+            dt = dateutil.parser.parse(created)
+            print dt.strftime(fmt) + " -- Issue created: " + title
+        else:
+            comments = s.get(issue['comments_url'], auth=auth)
+            comments = comments.json()
+            for comment in comments:
+                if comment['user']['login'] == auth[0]:
+                    created = comment['created_at']
+                    dt = dateutil.parser.parse(created)
+                    msg = dt.strftime(fmt) + " -- commented on issue: " + \
+                          title
+                    print msg
+
+
 def get_user_actor(auth):
+    print "="*25 + " Private events " + "="*25
     s = requests.session()
     url = "https://api.github.com/feeds"
     r = s.get(url, auth=auth)
@@ -35,11 +66,13 @@ def get_user_actor(auth):
         dt = datetime.datetime.fromtimestamp(time.mktime(published))
         print dt.strftime(fmt) + " -- " + title
 
+
 def get_user_events(auth):
     s = requests.session()
     url = "https://api.github.com/users/{}/events".format(auth[0])
     r = s.get(url,auth=auth)
     print len(r.json)
+
 
 def main():
     import keyring
@@ -48,10 +81,14 @@ def main():
         password = getpass('Password: ')
         keyring.set_password("github",username, password)
     elif "--user" in sys.argv:
-        username = raw_input("Username: ").strip()
+        if len(sys.argv) == 3:
+            username = sys.argv[2]
+        else:
+            username = raw_input("Username: ").strip()
         password = keyring.get_password("github",username)
         
     get_user_actor((username, password))
+    get_issues_data("enthought",(username, password))
 
 if __name__ == "__main__":
     main()
